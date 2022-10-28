2cce5350cd79e58060b02819f55b671584195e87
thiagolluiz <delimaluiz.thiago@gmail.com>
2022-10-28 11:35:50 -0300

    Deploy

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { users } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 import { UpdateUserDTO } from './dto/updateUser.dto';
 
 @Injectable()
 export class UsersService {
-  async create(): Promise<string> {
-    return 'Usuário criado com sucesso!';
+  constructor(private prisma: PrismaService){}
+
+  async create(data): Promise<users> {
+    const {name, email, password} = data;
+    const user = await this.prisma.users.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          message: 'Erro ao criar usuário!',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    return user;
   }
 
   async findAll(): Promise<string> {
